f8d3045e1709e90acde79b3eb0b294868700919a
PeasfulTown <peasfultown@gmail.com>
2026-05-14 14:48:51 +0700

    removed import

--- src/main/java/xyz/peasfultown/test/elasticsearch_database_simple_test/ArticleSearchService.java
+++ src/main/java/xyz/peasfultown/test/elasticsearch_database_simple_test/ArticleSearchService.java
@@ -10,7 +10,6 @@ import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
